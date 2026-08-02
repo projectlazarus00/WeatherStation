8258b291ac8afca31a5a5a6a0c215d389a2e023c
projectlazarus00 <projectlazarus00@gmail.com>
2026-08-02 19:55:35 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("BOOT OK - minimal test running");
+  Serial.println("Starting TFT test...");
+
+  tft.init();
+  Serial.println("TFT init done");
+  tft.setRotation(1);
+  tft.fillScreen(TFT_RED);
+  Serial.println("Screen should be RED now");
 }
 
 void loop() {
