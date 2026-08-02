59715a7109a5293b192dd40c3bfc3527cb0d274c
projectlazarus00 <projectlazarus00@gmail.com>
2026-08-02 20:18:55 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,21 @@
 #include <Arduino.h>
-#include <TFT_eSPI.h>
 #include <WiFiManager.h>
-
-TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  Serial.println("Starting TFT + WiFiManager test...");
-
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLUE);
-  Serial.println("Screen should be BLUE now");
+  delay(2000);
+  Serial.println("Test starting...");
 
   WiFiManager wm;
-  Serial.println("About to start WiFiManager portal...");
+  Serial.println("WiFiManager object created OK");
+
   bool res = wm.autoConnect("WeatherStation-Test");
 
+  Serial.println("autoConnect() returned");
   if (res) {
     Serial.println("WiFi connected!");
-    tft.fillScreen(TFT_GREEN);
   } else {
-    Serial.println("WiFi NOT connected, portal timed out or failed");
-    tft.fillScreen(TFT_YELLOW);
+    Serial.println("WiFi NOT connected");
   }
 }
 
